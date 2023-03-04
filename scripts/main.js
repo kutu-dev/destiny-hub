a79f010a810f2d7606e97e150cdcf8e9d92c3623
Kutu <the.kutu.dev@gmail.com>
2023-03-04 23:07:58 +0100

    Format files

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,55 @@
 // Random guardian text color
 
-const randomGuardianText = document.getElementById('randomGuardianClass');
+const randomGuardianText = document.getElementById("randomGuardianClass");
 const guardianClassesArray = [
-    'gradient-text--solar',
-    'gradient-text--void',
-    'gradient-text--arc',
-    'gradient-text--stasis',
-    'gradient-text--strand'
+  "gradient-text--solar",
+  "gradient-text--void",
+  "gradient-text--arc",
+  "gradient-text--stasis",
+  "gradient-text--strand",
 ];
 
 // Select a random number between 1 and 3
-const randomIndexClass = Math.floor(Math.random() * guardianClassesArray.length);
+const randomIndexClass = Math.floor(
+  Math.random() * guardianClassesArray.length
+);
 
 randomGuardianText.classList.add(guardianClassesArray[randomIndexClass]);
 
 // Random joke below the greetings
 
-const jokeQuote = document.getElementById('jokeQuote');
+const jokeQuote = document.getElementById("jokeQuote");
 const jokeQuoteArray = [
-    'Days since the last Telesto bug: 0',
-    '"A good hunter never dies" - Cayde-6',
-    'The current meta is: [Insert shitty weapon]',
-    'Doritos vs Cheetos Balls 2',
-    'Remember, Osiris is bald',
-    '"Holy shit, the game is worse than before" - Average Destiny player',
-    'Ikora... Who is that?',
-    'An MIT investigation indicates that the titans have Crayola as their favorite company',
-    'I hate Destiny 2, it\'s my favorite game',
-    'Fun fact: Ikora has the longest haircut out the 3 vanguards'
-]
+  "Days since the last Telesto bug: 0",
+  '"A good hunter never dies" - Cayde-6',
+  "The current meta is: [Insert shitty weapon]",
+  "Doritos vs Cheetos Balls 2",
+  "Remember, Osiris is bald",
+  '"Holy shit, the game is worse than before" - Average Destiny player',
+  "Ikora... Who is that?",
+  "An MIT investigation indicates that the titans have Crayola as their favorite company",
+  "I hate Destiny 2, it's my favorite game",
+  "Fun fact: Ikora has the longest haircut out the 3 vanguards",
+];
 
 const randomIndexQuote = Math.floor(Math.random() * jokeQuoteArray.length);
 jokeQuote.textContent = `${jokeQuoteArray[randomIndexQuote]}`;
 
 // Set favicon according the color mode
 
-const usesDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-const favicon = document.getElementById('favicon');
+const usesDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const favicon = document.getElementById("favicon");
 
 function setFavicon(isDarkMode) {
-    if (isDarkMode) {
-        favicon.href = './images/favicon-dark-mode.ico'
-    } else {
-        favicon.href = './images/favicon-white-mode.ico'
-    }
+  if (isDarkMode) {
+    favicon.href = "./images/favicon-dark-mode.ico";
+  } else {
+    favicon.href = "./images/favicon-white-mode.ico";
+  }
 }
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener(
-    "change", event => setFavicon(event.matches));
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => setFavicon(event.matches));
 
 setFavicon(usesDarkMode);
